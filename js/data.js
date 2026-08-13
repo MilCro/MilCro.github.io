55f17b9ee6d5fac6bec6ed481e5c391032ad3541
Millie C <milliecroucher99@gmail.com>
2026-08-13 12:34:01 +0100

    Some updates

---
+++ js/data.js
@@ -0,0 +1,72 @@
+
+const portfolioData = {
+  name: "Millie Rootes",
+  title: "2nd Year Computer Science student - Uni of Bath",
+  about:
+    "ahhhhhhhhhhhhhhhhhhhhhhhhhhhhh",
+  avatarUrl: "", // leave empty to hide avatar - no demo SVG will be shown
+
+  social: {
+    github: "https://github.com/MilCro",
+    linkedin: "https://www.linkedin.com/in/millie-rootes-384560307/",
+    website: "",
+  },
+
+  skills: [
+    "Python",
+    "Java",
+    "Haskell",
+    "OSINT",
+  ], 
+
+  projects: [
+    {
+    title: "Sentiment Analysis",
+    description:
+    "Uses Naive Bayes to predict the sentiment of a given piece of text.",
+    tags: ["Python","Naive Bayes"],
+    link: "",
+    repo: "",
+    image: "",
+    }, 
+  ],   
+
+  experience: [
+    // {
+    //   role: "",
+    //   company: "",
+    //   url: "",
+    //   start: "",
+    //   end: "",
+    //   location: "",
+    //   description:
+    //     "",
+    //   highlights: [
+    //     "",
+    //     "",
+    //   ],
+    // },
+  ],
+
+  education: [
+    {
+    school: "University of Bath",
+    degree: "B.Sc. Computer Science",
+    start: "2025",
+    end: "2028",
+    location: "Bath, UK",
+    firstYearResult: "76%",
+    highlights: ["Member of BCCS", "Member of CyberSoc"],
+    },
+  ],
+
+  contact: {
+    email: "https://www.youtube.com/watch?v=XfELJU1mRMg",
+    phone: "",
+    linkedin: "https://www.linkedin.com/in/millie-rootes-384560307/",
+    github: "https://github.com/MilCro",
+  },
+
+};
+
+export default portfolioData;
